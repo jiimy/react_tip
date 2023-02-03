e8aae4676559da1515b421644bb78f9bf84bd506
aiden <rudwnok123@naver.com>
2023-02-04 00:13:10 +0900

    Create style.js

---
+++ style.js
@@ -0,0 +1,7 @@
+export const Title = styled.div(({ theme }) => css`
+  margin-top: 48px;
+  font-family: ${theme.font};
+  font-size: ${theme.sizeBig};
+  color: ${theme.dark};
+  font-weight: ${theme.fontWeight}
+`)
