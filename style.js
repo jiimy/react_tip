f1e781b79c83c30070baf88826e6fbe838edeb80
aiden <rudwnok123@naver.com>
2023-02-04 00:24:04 +0900

    Update style.js

--- style.js
+++ style.js
@@ -5,3 +5,9 @@ export const Title = styled.div(({ theme }) => css`
   color: ${theme.dark};
   font-weight: ${theme.fontWeight}
 `)
+
+
+
+// https://velog.io/@syoung125/CSS-%EB%B0%98%EC%9D%91%ED%98%95-%EC%9B%B9-%EB%94%94%EC%9E%90%EC%9D%B8-%EC%A0%81%EC%9A%A9%ED%95%98%EA%B8%B0
+
+// https://jsramblings.com/how-to-use-media-queries-with-styled-components/
