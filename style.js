5769da4994f402ee459f5a862ed4476598b6f737
aiden <rudwnok123@naver.com>
2023-02-04 00:24:22 +0900

    Update style.js

--- style.js
+++ style.js
@@ -11,3 +11,6 @@ export const Title = styled.div(({ theme }) => css`
 // https://velog.io/@syoung125/CSS-%EB%B0%98%EC%9D%91%ED%98%95-%EC%9B%B9-%EB%94%94%EC%9E%90%EC%9D%B8-%EC%A0%81%EC%9A%A9%ED%95%98%EA%B8%B0
 
 // https://jsramblings.com/how-to-use-media-queries-with-styled-components/
+
+
+// https://tyhopp.com/notes/styled-components-media-queries
